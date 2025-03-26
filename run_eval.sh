0352f6a4bf6e3f50d64de70493dd07af453b0dd0
Matthew Farrell <matthew.farrell.235@gmail.com>
2025-03-26 15:19:39 +0900

    sync

--- run_eval.sh
+++ run_eval.sh
@@ -3,4 +3,5 @@ python phc/run_hydra.py learning=im_mcp_big exp_name=phc_comp_3 env=env_im_getup
 	env.num_prim=3 env.motion_file=sample_data/amass_isaac_standing_upright_slim.pkl \\
 	env.models=[output/HumanoidIm/phc_3/Humanoid.pth] env.num_envs=1 headless=True \\
 	im_eval=False env.num_envs=1 learning.params.config.minibatch_size=32 \\
-	learning.params.config.amp_minibatch_size=32
+	learning.params.config.amp_minibatch_size=32 num_threads=1 headless=False test=True \\
+	epoch=-1
