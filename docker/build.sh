444230a68a6e87420c37e1fb9fbb090ba09477f0
Matthew Farrell <matthew.farrell.235@gmail.com>
2025-03-26 17:32:45 +0900

    sync

--- docker/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-set -u
-SCRIPTROOT="$( cd "$(dirname "$0")" ; pwd -P )"
-cd "${SCRIPTROOT}/.."
-
-#docker build --no-cache --network host -t isaacgym -f docker/Dockerfile .
-docker build --network host -t isaacgym -f docker/Dockerfile .
