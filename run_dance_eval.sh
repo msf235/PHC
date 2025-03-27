3ef97703f69be308dde9280beb4c7b00cec48824
Matthew Farrell <matthew.farrell.235@gmail.com>
2025-03-27 14:18:26 +0900

    sync

--- run_dance_eval.sh
+++ run_dance_eval.sh
@@ -2,4 +2,4 @@ python phc/run_hydra.py project_name=Robot_IM robot=unitree_g1 \
   env=env_im_g1_phc env.motion_file=sample_data/dance_sample_g1.pkl \
   learning=im_pnn_big exp_name=unitree_g1_pnn sim=robot_sim control=robot_control \
   learning.params.network.space.continuous.sigma_init.val=-1.7 \
-  num_threads=1 headless=False test=True epoch=-1
+  num_threads=1 headless=False test=True env.num_envs=1 epoch=7500
