9d4c2f6f6d117787067a40d8a62fded01460b03d
Matthew Farrell <matthew.farrell.235@gmail.com>
2025-04-08 16:59:26 +0900

    sync

--- run_single_amass_example.sh
+++ run_single_amass_example.sh
@@ -1,4 +1,3 @@
-python phc/run_hydra.py learning=im_mcp_big exp_name=phc_comp_3 env=env_im_getup_mcp \
-  robot=smpl_humanoid env.zero_out_far=False robot.real_weight_porpotion_boxes=False \
-  env.motion_file=./sample_data/amass_single_example.pkl \
-  env.models=[output/HumanoidIm/phc_3/Humanoid.pth] headless=True
+python phc/run_hydra.py robot=smpl_humanoid \
+  env=env_im_getup_mcp env.motion_file=sample_data/amass_single_example.pkl \
+  learning=im_mcp_big exp_name=single_example 
