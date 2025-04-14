db9ae113d41047675c99f286c594f9a1c5d54561
Matthew Farrell <matthew.farrell.235@gmail.com>
2025-04-14 19:20:12 +0900

    sync

--- run_standing.sh
+++ run_standing.sh
@@ -1,4 +1,2 @@
-python phc/run_hydra.py learning=im_mcp_big exp_name=standing env=env_im_getup_mcp \\
-robot=smpl_humanoid env.zero_out_far=False robot.real_weight_porpotion_boxes=False \\
-env.motion_file=sample_data/amass_isaac_standing_upright_slim.pkl \\
-env.models=[output/HumanoidIm/phc_3/Humanoid.pth] headless=True \\
+python phc/run_hydra.py learning=im_mcp_big exp_name=standing env=env_im_getup_mcp robot=smpl_humanoid \
+	env.motion_file=sample_data/amass_isaac_standing_upright_slim.pkl env.models=[output/HumanoidIm/phc_3/Humanoid.pth] headless=True
