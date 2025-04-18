b2df9e9353ca796ac726f052751ed558703f80ca
Matthew Farrell <matthew.farrell.235@gmail.com>
2025-04-18 16:38:18 +0900

    sync

--- run_single_amass_example.sh
+++ run_single_amass_example.sh
@@ -1,3 +1,3 @@
 python phc/run_hydra.py robot=smpl_humanoid \
   env=env_im_getup_mcp env.motion_file=sample_data/amass_single_example.pkl \
-  learning=im_mcp_big exp_name=single_example 
+  learning=im_mcp_big exp_name=single_example
