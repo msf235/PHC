1cedf7d7ffcc17ccc6c6d45ae07fcc72061066f1
Matthew Farrell <matthew.farrell.235@gmail.com>
2025-04-18 15:44:49 +0900

    sync

--- run_test_processed.sh
+++ run_test_processed.sh
@@ -1,4 +1,4 @@
 python phc/run_hydra.py learning=im_mcp_big exp_name=test_mujoco env=env_im_getup_mcp \
   robot=smpl_humanoid env.zero_out_far=False robot.real_weight_porpotion_boxes=False \
   env.motion_file=./sample_data/test_processed_smpl_data.pkl \
-  env.models=[output/HumanoidIm/phc_3/Humanoid.pth] headless=True
+  headless=True
